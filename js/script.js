22a48a9286dac820c71a025ddcb8d2b7c0c61a6b
chidorishar <13747383+chidorishar@users.noreply.github.com>
2022-05-03 12:53:36 +0300

    Initial commit - added HW4 files

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const filters = document.querySelectorAll(".list-filters .button");
+var selectedCards = document.querySelectorAll(".card");
+
+filters.forEach((f) => f.addEventListener("click", onFiltersClick));
+
+function onFiltersClick() {
+  // unhide previously hidden cards
+  selectedCards.forEach((card) => card.classList.remove("hidden"));
+  if (this.id != "all-filter-button") {
+    //select cards by type given by clicked filter
+    selectedCards = document.querySelectorAll(
+      ".card:not(." + this.id.replace("-filter-button", "-card") + ")"
+    );
+    //hide selected cards
+    selectedCards.forEach((card) => card.classList.add("hidden"));
+  }
+  //set proper style to active filter button
+  document.querySelector(".active-filter").classList.remove("active-filter");
+  this.classList.add("active-filter");
+}
